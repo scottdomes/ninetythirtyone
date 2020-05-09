99c218f2240ce7992f1d6a12158024681ffff8ca
scottdomes <scott@musefind.com>
2020-05-09 11:37:10 -0700

    Display 90 day goals

--- src/screens/DayView.js
+++ src/screens/DayView.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  ActivityIndicator,
+} from 'react-native';
+
 import * as firebase from 'firebase';
 
+const todaysDate = () => {
+  const month = new Date().getMonth() + 1;
+  const year = new Date().getFullYear();
+  const date = new Date().getDate();
+  return `${year}-${month}-${date}`;
+};
+
 export default class DayView extends React.Component {
-  state = { loaded: false };
+  state = { loaded: false, goals: {} };
   componentDidMount() {
+    this.checkForUser();
+  }
+
+  checkForUser() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.setState({ loaded: true });
+        this.checkForTodaysData(user.uid);
       } else {
         this.props.navigation.navigate('PhoneEntry');
       }
     });
   }
 
+  async checkForTodaysData(userId) {
+    const date = todaysDate();
+    console.log(userId, date);
+    const goals = await firebase
+      .database()
+      .ref(`/users/${userId}/${date}`)
+      .once('value');
+    this.setState({ loaded: true, goals: goals.val() });
+  }
+
   render() {
+    if (!this.state.loaded) {
+      return <View style={styles.activityIndicatorContainer}>
+        <ActivityIndicator size="large" color="#3F5EFB" />
+      </View>;
+    }
+
+    const ninetyDayGoals = this.state.goals.ninety;
     return (
       <View>
-        <Text>Did it!</Text>
+        {ninetyDayGoals.map((goal) => {
+          return <Text key={goal}>{goal}</Text>;
+        })}
+
         <TouchableWithoutFeedback onPressIn={() => firebase.auth().signOut()}>
           <Text>Logout</Text>
         </TouchableWithoutFeedback>
@@ -25,3 +63,24 @@ export default class DayView extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 15,
+    position: 'relative',
+  },
+  activityIndicatorContainer: {
+    position: 'absolute',
+    flex: 1,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 2,
+  },
+});
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,20 +1,21 @@
 import * as firebase from 'firebase';
+import { FIREBASE_API_KEY, FIREBASE_MESSAGING_SENDER_ID } from '../../secrets';
 
 // Optionally import the services that you want to use
 import 'firebase/auth';
-//import "firebase/database";
+import 'firebase/database';
 //import "firebase/firestore";
 //import "firebase/functions";
 //import "firebase/storage";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: 'AIzaSyBHEo9lh0MzzPaOBQYVr2sGWIYvp63zr4Y',
+  apiKey: FIREBASE_API_KEY,
   authDomain: 'ninetythirtyone-f41bf.firebaseapp.com',
   databaseURL: 'https://ninetythirtyone-f41bf.firebaseio.com',
   projectId: 'ninetythirtyone-f41bf',
   storageBucket: 'ninetythirtyone-f41bf.appspot.com',
-  messagingSenderId: '845224636952',
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   appId: '1:845224636952:web:c96dc1f696fadc2f2d096d',
   measurementId: 'G-PZZEZVF4PG',
 };
